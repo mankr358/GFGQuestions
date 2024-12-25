507047574a0c6be27a1f71e4aa1ef7cdc90d9222
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-12-25 08:11:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine(); // Use lowercase 's'
+
+            Solution ob = new Solution();
+            if (ob.isPalindrome(s)) // Check palindrome
+                System.out.println("true");
+            else
+                System.out.println("false");
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+       char[] ch = s.toCharArray();
+        int i =0;
+        int j = s.length()-1;
+       while(i<j){
+           if(ch[i] != ch[j]){
+               return false;
+           }
+           i++;
+           j--;
+       }
+       return true;
+        
+    }
+}
